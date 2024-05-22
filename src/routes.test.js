df6dae3b581fc47bd9c43b1f448a163352a9f1f0
Bruno Tenório <tenoriobn@gmail.com>
2024-05-22 18:31:33 -0300

    Controlando o histórico

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from './paginas/Principal/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Routes, Route } from "react-router-dom";
+import Cartoes from './componentes/Cartoes'
 
 describe('Rotas', () => {
   test('Deve renderizar a rota principal', () => {
@@ -8,5 +9,22 @@ describe('Rotas', () => {
 
     const usuario = screen.getByText('Olá, Joana :)!')
     expect(usuario).toBeInTheDocument();
+  });
+
+  test('Deve renderizar a rota Cartões', () => {
+    const rota = '/cartoes';
+
+    render(
+      <MemoryRouter initialEntries={[rota]}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path='cartoes' element={<Cartoes />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const meusCartoes = screen.getByText('Meus cartões');
+    expect(meusCartoes).toHaveTextContent('Meus cartões');
   })
 })
