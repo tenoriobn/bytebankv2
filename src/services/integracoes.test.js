e01dc681be13a06e7d441543077d42ae46883a07
Bruno Tenório <tenoriobn@gmail.com>
2024-05-31 20:43:43 -0300

    criando mocks de chamada

--- src/services/integracoes.test.js
+++ src/services/integracoes.test.js
@@ -1,15 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from '../paginas/Principal/App';
+import api from './api'
 import { buscaTransacoes } from './transacoes';
+
+jest.mock('./api');
+
+const mockTransacao = [
+  {
+    id: 1,
+    transacao: 'Depósito',
+    valor: '100',
+    data: '22/11/2022',
+    mes: 'novembro',
+  },
+];
+
+const mockRequisicao = (retorno) => {
+  return new Promise ((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno
+      });
+    }, 200)
+  });
+};
 
 describe('Requisições para API', () => {
   test('Deve retornar uma lista de transações', async () => {
-    const transacoes = await buscaTransacoes();
-    expect(transacoes).toHaveLength(3);
+    api.get.mockImplementation(() => mockRequisicao(mockTransacao));
 
-    render(<App />, {wrapper: BrowserRouter});
-    const todasTransacoes = await screen.findAllByText('Novembro');
-    todasTransacoes.forEach(transacao => expect(transacao).toBeInTheDocument());
-  });
+    const transacoes = await buscaTransacoes();
+    expect(transacoes).toEqual(mockTransacao);
+
+    expect(api.get).toHaveBeenCalledWith('/transacoes');
+  })
 });
+
