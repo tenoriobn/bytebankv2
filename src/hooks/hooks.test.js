9f9c4233c77a4317c634d625217b2ccd6724bdfa
Bruno Tenório <tenoriobn@gmail.com>
2024-06-03 18:28:13 -0300

    Hooks do react

---
+++ src/hooks/hooks.test.js
@@ -0,0 +1,16 @@
+import { renderHook } from '@testing-library/react';
+import { useState, useEffect } from "react";
+
+test('Hooks', () => {
+  const { result } = renderHook(() => {
+    const [nome, setNome] = useState('');
+    
+    useEffect(() => {
+      setNome('Alice');
+    }, []);
+
+    return nome;
+  });
+
+  expect(result.current).toBe('Alice');
+})
